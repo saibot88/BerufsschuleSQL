9a9cf8c1bb05dc5ab95b675ee4112e06f9c69e79
saibot88 <tkaufmann.putzbrunn@gmail.com>
2022-05-12 12:12:11 +0200

    Update AUF-07-1-4.sql
    New Aufgaben

--- files/AUF-07-1-4.sql
+++ files/AUF-07-1-4.sql
@@ -9,3 +9,15 @@ From mitarbeiter m INNER JOIN abteilung a ON a.id = m.aid;
 SELECT m.name,m.vorname,m.aid,a.id,a.name
 From mitarbeiter m, abteilung a
 WHERE m.aid==a.id;
+
+SELECT m.name,m.vorname,g.gehalt,m.strasse,o.plz,o.name
+From 
+mitarbeiter m 
+INNER JOIN gehalt g ON g.id = m.fid 
+INNER JOIN ort o ON o.id = m.oid;
+
+SELECT name as 'name', telefon as 'telefonnummer'
+From kunde
+UNION
+SELECT name as 'name', telefon as 'telefonnummer'
+FROM lieferant
